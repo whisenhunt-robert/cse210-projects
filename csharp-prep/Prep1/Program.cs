c10a7c1ed5dd55f67e69b919ffa74e1d830eddb8
Robert Whisenhunt <dahweirdacus@gmail.com>
2025-01-11 20:42:25 -0500

    Completed Prep 1 and 2.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        // Ask the user for their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask the user for their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Prints the users name in a James Bond style (i.e. Bond, James Bond)
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        // Ask the user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        // Initialize the letter grade variable
+        string letter = "";
+
+        // Determine the letter grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Print the letter grade
+        Console.WriteLine($"Grade: {letter}");
+
+        // Check if the user passed or not
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, but that's not a passing grade! Don't give up! Retake the course and try again!");
+        }
     }
 }
